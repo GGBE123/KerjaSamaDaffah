fc9d19769941057715e88e7a0d93795121f758bd
Danielandersontech <daniel23ti@mahasiswa.pcr.ac.id>
2024-11-14 11:53:36 +0700

    asdasda

---
+++ GajahTerbang.c
@@ -0,0 +1,18 @@
+asdasdas a aa
+d
+a
+s as
+d a
+
+sda
+_USE_DECLSPECS_FOR_SALd asmd as
+asm asms
+_USE_DECLSPECS_FOR_SALd as
+ as
+  a
+  __STDC_NO_ATOMICS__ as
+   as
+    
+    d 
+    asd 
+    asmd as
