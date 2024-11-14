c5ad29032b29b1a0b108cc7739a289005361d792
abidarkan <abid23ti@mahasiswa.pcr.ac.id>
2024-11-14 11:55:33 +0700

    asdasdasdasdas

--- GajahTerbang.c
+++ GajahTerbang.c
@@ -7,7 +7,7 @@ d a
 sda
 _USE_DECLSPECS_FOR_SALd asmd as
 asm asms
-_USE_DECLSPECS_FOR_SALd as
+_USE_DECLSPECS_FOR_SALd asasdasdasdasddasdasdasdassdsaddadadasdasdasdasdad
  as
   a
   __STDC_NO_ATOMICS__ as
